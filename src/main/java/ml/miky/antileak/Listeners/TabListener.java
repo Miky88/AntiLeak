1c88ce4f6cd9f8b4ecc5e1cea546955cae8bb1dc
Miky88 <mikyottantotto.88@gmail.com>
2021-03-16 12:38:54 +0100

    Something went wrong idk

--- src/main/java/ml/miky/antileak/Listeners/TabListener.java
+++ src/main/java/ml/miky/antileak/Listeners/TabListener.java
@@ -1,4 +1,21 @@
 package ml.miky.antileak.Listeners;
 
-public class TabListener {
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerCommandSendEvent;
+
+import java.util.List;
+
+import static ml.miky.antileak.AntiLeak.instance;
+
+public class TabListener implements Listener {
+
+    @EventHandler(priority = EventPriority.HIGHEST)
+    public void onTabComplete(PlayerCommandSendEvent e) {
+        if(e.getPlayer().hasPermission("antileak.bypass.tab")) return;
+        List<String> commands = instance.getConfig().getStringList("whitelist");
+        e.getCommands().removeIf(cmd -> !commands.contains(cmd));
+    }
+
 }
